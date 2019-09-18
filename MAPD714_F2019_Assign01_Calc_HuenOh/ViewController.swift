5659c1596265e538b96ad0e922dbcb90e37bedfd
HUEN_OH <heons921@gmail.com>
2019-09-18 19:22:05 -0400

    Initial Commit

---
+++ MAPD714_F2019_Assign01_Calc_HuenOh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MAPD714_F2019_Assign01_Calc_HuenOh
+//
+//  Created by HUEN_OH on 2019-09-18.
+//  Copyright © 2019 Centennial College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
